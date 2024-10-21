e18a172269e186b0133fd6a7cfbff91cc1e9d7dd
KHAN IMRAN <imrankhanshakeel012@gmail.com>
2024-10-22 04:16:06 +0530

    "Added classes to input fields, updated script.js to handle checkbox clicks and update progress bar, quote, and completed text accordingly."

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const errorLabel = document.querySelector('.error-label');
 const checkboxAll = document.querySelectorAll('.custom-checkbox');
 const input = document.querySelectorAll('#task');
 const quote = document.querySelector('#quote');
-const progressBar = document.querySelector('.complete-bar')
-const completedText = document.querySelector('#completed-text')
+const progressBar = document.querySelector('.complete-bar');
+const completedText = document.querySelector('#completed-text');
 
+const quoteArr = [
+    'Move one step ahead, today!',
+    'Great start! One step closer to your goal.',
+    'Almost there! Just one more to go!',
+    "Congratulations! You've completed all your goals for today!",
+];
 
 checkboxAll.forEach((checkbox) => {
     checkbox.addEventListener('click', (e) => {
@@ -14,41 +20,46 @@ checkboxAll.forEach((checkbox) => {
         if (allInputField) {
             checkbox.parentElement.classList.toggle('completed');
 
-            const completedTasks = document.querySelectorAll('.completed').length;
+            progressBar.classList.add('complete-progress');
 
-            progressBar.classList.add('complete-progress')
-            
-            input.forEach((el)=>{
+            input.forEach((el) => {
                 if (el.parentElement.classList.contains('completed')) {
-                    el.setAttribute('readonly', true)
-                }else{
-                    el.removeAttribute('readonly')
+                    el.setAttribute('readonly', true);
+                } else {
+                    el.removeAttribute('readonly');
                 }
-                
-            })
+            });
 
+            const completedTasks = document.querySelectorAll('.completed').length;
+            progressBar.style.width = `${(completedTasks / 3) * 100}%`;
             if (completedTasks === 0) {
-                progressBar.style.width = '0%';
-                completedText.innerText = ''
-                quote.innerText = 'Move one step ahead, today!';
+                completedText.innerText = '';
+            } else {
+                completedText.innerText = `${completedTasks}/3 Completed`;
             }
-            else if (completedTasks === 1) {
-                progressBar.style.width = '33.33%';
-                completedText.innerText = '1/3 Completed' 
-                quote.innerText = '"Great start! One step closer to your goal."';
+            quote.innerText = quoteArr[completedTasks];
 
-            }
-            else if (completedTasks === 2) {
-                progressBar.style.width = '66.66%';
-                completedText.innerText = '2/3 Completed'
-                quote.innerText = '"Almost there! Just one more to go!"';
-            }
-            else if (completedTasks === 3) {
-                progressBar.style.width = '100%'
-                completedText.innerText = '3/3 Completed'
-                quote.innerText = `"Congratulations! You've completed all your goals for today!"`;
-            }
+            // if (completedTasks === 0) {
+            //     progressBar.style.width = '0%';
+            //     completedText.innerText = ''
+            //     quote.innerText = 'Move one step ahead, today!';
+            // }
+            // else if (completedTasks === 1) {
+            //     progressBar.style.width = '33.33%';
+            //     completedText.innerText = '1/3 Completed'
+            //     quote.innerText = '"Great start! One step closer to your goal."';
 
+            // }
+            // else if (completedTasks === 2) {
+            //     progressBar.style.width = '66.66%';
+            //     completedText.innerText = '2/3 Completed'
+            //     quote.innerText = '"Almost there! Just one more to go!"';
+            // }
+            // else if (completedTasks === 3) {
+            //     progressBar.style.width = '100%'
+            //     completedText.innerText = '3/3 Completed'
+            //     quote.innerText = `"Congratulations! You've completed all your goals for today!"`;
+            // }
         } else {
             errorLabel.style.visibility = 'visible';
         }
